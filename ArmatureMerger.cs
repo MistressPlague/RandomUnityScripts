acbad049785254e8e326e3d5ef36850257a001a5
Mistress Plague <36628963+MistressPlague@users.noreply.github.com>
2021-11-27 10:41:31 +0000

    Update ArmatureMerger.cs

--- ArmatureMerger.cs
+++ ArmatureMerger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -80,3 +81,4 @@ public class ArmatureInspector : Editor
         }
     }
 }
+#endif
